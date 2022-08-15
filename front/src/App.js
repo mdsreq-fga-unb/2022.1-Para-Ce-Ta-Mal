2c1bb114a27b0a82f049924a4acd9a01087da8d2
Joao Pedro <joaopedrocardosorod@gmail.com>
2022-08-15 20:28:51 -0300

    create search bar

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import { FaCashRegister } from 'react-icons/fa';
 import { FaBoxOpen } from 'react-icons/fa';
 import { FaUserTie } from 'react-icons/fa';
 import { FaChartLine } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import { GoGear } from "react-icons/go";
 import "./App.css";
 
@@ -11,39 +12,33 @@ function App() {
   return (
 
     <html>
-      <header>
-        <div class="topnav">
-          <a class="active" href="#home">ElectroPharm</a>
-        </div>
-      </header>
+      <head>
+        <meta charSet="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width initial-scale=1.0"></meta>
+          <title>ElectroPharm</title>
+      </head>
+       <div class="topnav">
+        <a class="active" href="#home">ElectroPharm</a>
+      </div>
+      <div class="sidebar">
+        <ul>
+          <li> <a href="#"> <i><FaShoppingCart class="icon-sidebar" /></i></a></li>
+          <li> <a href="#"> <i> <FaCashRegister class="icon-sidebar" /></i></a></li>
+          <li> <a href="#"> <i> <FaBoxOpen class="icon-sidebar" /></i></a></li>
+          <li> <a href="#"> <i> <FaUserTie class="icon-sidebar" /></i></a></li>
+          <li> <a href="#"> <i> <FaChartLine class="icon-sidebar" /></i></a></li>
+          <li> <a href="#"> <i> <GoGear class="icon-sidebar" /></i></a></li>
+        </ul>
+      </div>
       <body>
-        <div class="menu">
-            <div class="electron-back"> 
-                <div class="box">
-                  <FaShoppingCart class="icons" />
-                  <h3> Vender</h3>
-                </div>
-                <div class="box">
-                  <FaCashRegister class="icons" />
-                  <h3> Caixa</h3>
-                </div>
-                <div class="box">
-                  <FaBoxOpen class="icons" />
-                  <h3> Estoque</h3>
-                </div>  
-                <div class="box">
-                  <FaUserTie class="icons" />
-                  <h3> Gerência</h3>
-                </div>
-                <div class="box">
-                  <FaChartLine class="icons" />
-                  <h3> Financeiro</h3>
-                </div>
-                <div class="box">
-                  <GoGear class="icons" />
-                  <h3> Configurações</h3>
-                </div>
-            </div>
+        <div class="electron-back">
+          
+            <div class="search_box">
+              <input type="text" placeholder="Pesquise aqui"></input>
+              <i> <FaSearch class="icon-search" /></i>
+            
+          </div>
+
         </div>
       </body>
     </html>
