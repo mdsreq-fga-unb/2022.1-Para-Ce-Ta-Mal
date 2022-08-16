d38bb359f4abb768017095e282c665a2abf2cb64
Lucas Cardoso <cardosolucasrodrigues@gmail.com>
2022-08-15 23:15:57 -0300

    initial commit for routes configuration

--- front/src/index.js
+++ front/src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import Estoque from './pages/Estoque/Estoque.js';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
 
   </React.StrictMode>
+
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
