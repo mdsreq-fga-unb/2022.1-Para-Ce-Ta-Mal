6d8b61c2985edc247b32f5340f4f8ad8ef49d06a
barbosaoli <gustavobarbosa7162@gmail.com>
2022-08-15 23:36:44 -0300

    start react router dom

--- front/src/App.js
+++ front/src/App.js
@@ -1,49 +1,50 @@
 import React from "react";
-import { FaShoppingCart } from 'react-icons/fa';
-import { FaCashRegister } from 'react-icons/fa';
-import { FaBoxOpen } from 'react-icons/fa';
-import { FaUserTie } from 'react-icons/fa';
-import { FaChartLine } from 'react-icons/fa';
+import { FaShoppingCart } from "react-icons/fa";
+import { FaCashRegister } from "react-icons/fa";
+import { FaBoxOpen } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
 import { GoGear } from "react-icons/go";
 import "./App.css";
 
 function App() {
   return (
-
     <html>
       <header>
         <div class="topnav">
-          <a class="active" href="#home">ElectroPharm</a>
+          <a class="active" href="#home">
+            ElectroPharm
+          </a>
         </div>
       </header>
       <body>
         <div class="menu">
-            <div class="electron-back"> 
-                <div class="box">
-                  <FaShoppingCart class="icons" />
-                  <h3> Vender</h3>
-                </div>
-                <div class="box">
-                  <FaCashRegister class="icons" />
-                  <h3> Caixa</h3>
-                </div>
-                <div class="box">
-                  <FaBoxOpen class="icons" />
-                  <h3> Estoque</h3>
-                </div>  
-                <div class="box">
-                  <FaUserTie class="icons" />
-                  <h3> Gerência</h3>
-                </div>
-                <div class="box">
-                  <FaChartLine class="icons" />
-                  <h3> Financeiro</h3>
-                </div>
-                <div class="box">
-                  <GoGear class="icons" />
-                  <h3> Configurações</h3>
-                </div>
+          <div class="electron-back">
+            <div class="box">
+              <FaShoppingCart class="icons" />
+              <h3> Vender</h3>
             </div>
+            <div class="box">
+              <FaCashRegister class="icons" />
+              <h3> Caixa</h3>
+            </div>
+            <div to="/estoque" class="box">
+              <FaBoxOpen class="icons" />
+              <h3> Estoque</h3>
+            </div>
+            <div class="box">
+              <FaUserTie class="icons" />
+              <h3> Gerência</h3>
+            </div>
+            <div class="box">
+              <FaChartLine class="icons" />
+              <h3> Financeiro</h3>
+            </div>
+            <div class="box">
+              <GoGear class="icons" />
+              <h3> Configurações</h3>
+            </div>
+          </div>
         </div>
       </body>
     </html>
--- front/src/index.js
+++ front/src/index.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Estoque from './pages/Estoque/Estoque.js';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Estoque from "./pages/Estoque/Estoque.js";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/estoque" element={<Estoque />} />
+      </Routes>
     </BrowserRouter>
-
-
   </React.StrictMode>
-
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
