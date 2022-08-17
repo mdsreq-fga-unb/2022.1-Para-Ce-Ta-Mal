a3613bdbf98761b615a063b3759c5cb6700bf7be
yesklin <cd.santos.360@gmail.com>
2022-08-17 18:11:24 -0300

    routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Menu from './pages/Menu/Menu';
+import Vendas from "./pages/Vendas/Vendas";
+import Estoque from './pages/Estoque/Estoque';
+
 import "./App.css";
 
-function App() {
+const App = () => {
 
   return (
     <>
+      <HashRouter>
+        <Routes>
+          <Route path="/" exact element={ Menu } />
+          <Route path="/vendas"  element={ Vendas } />
+          <Route path="/estoque" element={ Estoque } />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
