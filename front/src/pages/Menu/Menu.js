2a8e6acb167a4aef614ff5cc161d2fac5d0e0c54
yesklin <cd.santos.360@gmail.com>
2022-08-17 19:02:59 -0300

    creating menu

--- front/src/pages/Menu/Menu.js
+++ front/src/pages/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { FaShoppingCart } from "react-icons/fa";
 import { FaCashRegister } from "react-icons/fa";
 import { FaBoxOpen } from "react-icons/fa";
@@ -6,48 +7,18 @@ import { FaUserTie } from "react-icons/fa";
 import { FaChartLine } from "react-icons/fa";
 import { GoGear } from "react-icons/go";
 import { useNavigate } from "react-router-dom";
+
 import "../../App.css";
+import "./Menu.css";
 
 function Menu() {
   const navigate = useNavigate();
 
   return (
     <>
-      <main>
-        <div className="menu">
-          <div className="electron-back">
-            <div className="box">
-              <FaShoppingCart className="icons" onClick={() => navigate("/vendas")}/>
-              <h3> Vender</h3>
-            </div>
-
-            <div className="box">
-              <FaCashRegister className="icons" />
-              <h3> Caixa</h3>
-            </div>
-
-            <div className="box" onClick={() => navigate("/estoque")}>
-              <FaBoxOpen className="icons" />
-              <h3> Estoque</h3>
-            </div>
-
-            <div className="box">
-              <FaUserTie className="icons" />
-              <h3> Gerência</h3>
-            </div>
-
-            <div className="box">
-              <FaChartLine className="icons" />
-              <h3> Financeiro</h3>
-            </div>
-
-            <div className="box">
-              <GoGear className="icons" />
-              <h3> Configurações</h3>
-            </div>
-          </div>
-        </div>
-      </main>
+      <div className="menu menu-container">
+        <div className="menu-button"></div>
+      </div>
     </>
   );
 }
