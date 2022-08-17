c93041e9aac879e1ca1d814cadc5cd63e8ab9ab5
yesklin <cd.santos.360@gmail.com>
2022-08-17 17:54:35 -0300

    create Menu page

---
+++ front/src/pages/Menu/Menu.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaCashRegister } from "react-icons/fa";
+import { FaBoxOpen } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
+import { GoGear } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
+import "../../App.css";
+
+function Menu() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <main>
+        <div className="menu">
+          <div className="electron-back">
+            <div className="box">
+              <FaShoppingCart className="icons" onClick={() => navigate("/vendas")}/>
+              <h3> Vender</h3>
+            </div>
+
+            <div className="box">
+              <FaCashRegister className="icons" />
+              <h3> Caixa</h3>
+            </div>
+
+            <div className="box" onClick={() => navigate("/estoque")}>
+              <FaBoxOpen className="icons" />
+              <h3> Estoque</h3>
+            </div>
+
+            <div className="box">
+              <FaUserTie className="icons" />
+              <h3> Gerência</h3>
+            </div>
+
+            <div className="box">
+              <FaChartLine className="icons" />
+              <h3> Financeiro</h3>
+            </div>
+
+            <div className="box">
+              <GoGear className="icons" />
+              <h3> Configurações</h3>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Menu;
