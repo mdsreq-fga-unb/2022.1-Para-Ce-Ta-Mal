30da83ac6eef4b68a4f8ab7c962d562b302ed3c9
Carlos Daniel <cd.santos.360@gmail.com>
2022-08-18 11:17:38 -0300

    Revert "Make components"

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,53 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-import Menu from './pages/Menu/Menu';
-import Vendas from "./pages/Vendas/Vendas";
-import Estoque from './pages/Estoque/Estoque';
-import Header from "./components/Header/Header";
-
-
+import { FaShoppingCart } from "react-icons/fa";
+import { FaCashRegister } from "react-icons/fa";
+import { FaBoxOpen } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
+import { GoGear } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
-const App = () => {
+function App() {
+  const navigate = useNavigate();
 
   return (
     <>
-      <div>
-        <Header/>
-        <HashRouter>
-          <Routes>
-            <Route path="/" exact element={ <Menu/> } />
-            <Route path="/vendas"  element={ <Vendas/>} />
-            <Route path="/estoque" element={ <Estoque/> } />
-          </Routes>
-        </HashRouter>
-      </div>
+      <main>
+        <div className="menu">
+          <div className="electron-back">
+            <div className="box">
+              <FaShoppingCart className="icons" />
+              <h3> Vender</h3>
+            </div>
 
+            <div className="box">
+              <FaCashRegister className="icons" />
+              <h3> Caixa</h3>
+            </div>
+
+            <div className="box" onClick={() => navigate("/estoque")}>
+              <FaBoxOpen className="icons" />
+              <h3> Estoque</h3>
+            </div>
+
+            <div className="box">
+              <FaUserTie className="icons" />
+              <h3> Gerência</h3>
+            </div>
+
+            <div className="box">
+              <FaChartLine className="icons" />
+              <h3> Financeiro</h3>
+            </div>
+
+            <div className="box">
+              <GoGear className="icons" />
+              <h3> Configurações</h3>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
